67f9357954b2cd64fbae88830d3ef08fe1a68ba8
sbcreates <drawsarahb@gmail.com>
2017-10-12 12:42:42 -0400

    Added synchronous form that will submit votes to API

--- election.js
+++ election.js
@@ -5,20 +5,27 @@ document.addEventListener("DOMContentLoaded", function() {
   $.ajax ({
     url: "https://bb-election-api.herokuapp.com/",
     method: 'GET',
-    // data:
     datatype: 'json'
   }).done(function(data){
-    // data['candidates'].forEach(function(candidate, index){
-    //
-    //     var $li = $('<li>', {class: 'candidate' + (index + 1)}).text('Name: ' + candidate[i]["name"] + '  |  Votes: ' + candidate[i]["votes"]);
-    //
-    //     $('#candidate-list').append($li)
-    // })
+    
     for (var i = 0; i < data['candidates'].length; i++){
 
       var $li = $('<li>', {class: 'candidate' + (i + 1)}).text('Name: ' + data["candidates"][i]["name"] + '  |  Votes: ' + data["candidates"][i]["votes"]);
 
       $('#candidate-list').append($li)
-      }
-    });
+
+      var $form = $('<form>', {class: 'candidate' + (i + 1) + '-form', method: 'POST', action: 'https://bb-election-api.herokuapp.com/vote'});
+
+      $('.candidate' + (i + 1)).append($form);
+
+      var $submit = $('<input>', {type: 'submit', id: 'submit', value: 'Vote for me!'});
+
+      var $hidden = $('<input>', {type: 'hidden', name: 'id', value: data["candidates"][i]['id']});
+
+      $('.candidate' + (i + 1) + '-form').append($hidden).append($submit);
+
+    };
   });
+
+
+});
